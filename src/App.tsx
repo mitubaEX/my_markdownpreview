4449800ce279e77877e8d578f5377ad2417740a6
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2021-04-24 22:33:43 +0900

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ const App = () => {
 
   return (
     <div style={{ display: 'flex' }}>
-      <div style={{ width: '50%', height: '80vh' }}>
+      <div style={{ width: '50%', height: '95vh' }}>
         <textarea style={{ width: '98%', height: '100%' }} value={textAreaValue} onChange={(e) => setTextAreaValue(e.currentTarget.value)} />
       </div>
-      <div style={{ width: '50%', height: '80vh' }}>
-        <div style={{ width: '100%', height: '100%', border: '1px solid' }} className="markdown-body">
+      <div style={{ width: '50%', height: '95vh', overflow: 'scroll' }}>
+        <div style={{ width: '100%', height: '100%' }} className="markdown-body">
           <ReactMarkdown remarkPlugins={[gfm]}>
             {textAreaValue}
           </ReactMarkdown>
